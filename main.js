b409570e0ec42aaa26f0586b51211a9461a8d4c4
Saefulloh Maslul <maslulsaefulloh@gmail.com>
2022-07-04 21:29:16 +0700

    buat function di javascript

---
+++ main.js
@@ -0,0 +1,19 @@
+const someFunc = (someParam) => {
+    console.log(someParam)
+}
+
+someFunc("bakso")
+
+const sendMessageByWa = (phone, text, image) => {
+    // logic
+    // kirim image
+    console.log(image)
+}
+
+sendMessageByWa("08xxx", "hoy makan malem yuk")
+
+sendMessageByWa("08xxx", "hoy makan malem yuk")
+
+sendMessageByWa("08xxx", "hoy makan malem yuk")
+
+sendMessageByWa("08xxx", "hoy makan malem yuk")
